ad4753f26b2395eb0c49967fbee0efe42f046587
Tanaya0914 <89084603+Tanaya0914@users.noreply.github.com>
2023-02-06 01:14:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var result = document.getElementById("screen");
+
+let calculate = (number) => {
+    result.value += number;
+}
+
+let Result = () => {
+    try {
+        result.value = eval(result.value);
+    } catch (err) {
+        alert("Enter valid input ☹️☹️!!")
+    }
+}
+
+function clr() {
+    result.value = "";
+}
+
+function del() {
+
+    result.value = result.value.slice(0, -1);
+}
